721633e29b617c08eff118f902399c894bf52039
Alexey Fedorenko <messer.ua@gmail.com>
2019-11-28 22:10:12 +0200

    header

---
+++ src/components/header/header.js
@@ -0,0 +1,43 @@
+import React from "react";
+import "./css/style.css";
+import Message from "./message";
+import "./assets/menu.png";
+
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="header">
+        <div className="top">
+          <div className="smart">
+            <div className="logo">
+              <span className="upper">S</span>mart
+            </div>
+          </div>
+          <div className="menu">
+            <img src="menu.png" alt=""></img>
+          </div>
+        </div>
+        <div className="center">
+          <h1>
+            <span className="smart">Smart </span>Agency Landing Page
+          </h1>
+          <p className="text">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores,
+            fuga provident. Libero magni quibusdam labore ad iste saepe hic,
+            maxime qui ipsum tempore beatae, volu tomi ptates doloremque rerum
+            aspernatur dolorum at!
+          </p>
+        </div>
+        <div className="bottom">
+          <Message></Message>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Header;
---
+++ src/components/header/message.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./css/style.css";
+
+class Message extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="message">
+        <h3 className="test">
+          <span className="send">Send </span>A Message
+        </h3>
+        <p className="input">Your Name</p>
+        <div className="line"></div>
+        <p className="input">Your Email</p>
+        <div className="line"></div>
+        <p className="mess">Your Message</p>
+        <div className="mess-type"></div>
+        <a href="#">Send Message</a>
+      </div>
+    );
+  }
+}
+
+export default Message;
